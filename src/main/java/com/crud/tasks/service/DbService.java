bf7213c5c1cff122656f242407f390a1984d16bc
Alicja-Jot <miedzinska93@gmail.com>
2020-03-18 20:29:19 +0100

    Utwórz metodę do wyszukiwania pojedynczego zadania

---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DbService {
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> getAllTasks(){
+        return repository.findAll();
+    }
+
+    public Task getTask(Long taskId) {
+        Optional<Task> optionalTask = repository.findById(taskId);
+
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            return task;
+        } else {
+            return null;
+        }
+    }
+}
